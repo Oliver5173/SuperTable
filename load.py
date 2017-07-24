ddefcd6841d5000611ca24d093c24eb3e6f70cab
Alex Li <li1995717@gmail.com>
2017-07-24 17:15:23 -0400

    add new function with multi input course

--- load.py
+++ load.py
@@ -4,9 +4,12 @@ import json
 SFU_API = "http://www.sfu.ca/bin/wcm/academic-calendar?{year}/{term}/courses/{paramemters}"
 YEAR = input("YEAR: ?")
 TERM = input("TERM: ?")
-PARAMEMTERS = input("COURSE#: ?").lower()
-
-NEW_QUERY = SFU_API.format(year=YEAR, term=TERM, paramemters=PARAMEMTERS)
+PARAMEMTERS = input("COURSE#: ?").lower().split(" ")
+for PARAMEMTER in PARAMEMTERS:
+    print(PARAMEMTER)
+    NEW_QUERY = SFU_API.format(year=YEAR, term=TERM, paramemters=PARAMEMTER)
+    RES = requests.get(NEW_QUERY)
+    print(RES.json())
 print(NEW_QUERY)
 RES = requests.get(NEW_QUERY)
 print(RES.json())
