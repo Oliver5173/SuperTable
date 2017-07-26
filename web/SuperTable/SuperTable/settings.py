605465929227ade6766785dfbcbda055690b9251
Alex Li <li1995717@gmail.com>
2017-07-25 21:39:41 -0400

    add static folder and path

--- web/SuperTable/SuperTable/settings.py
+++ web/SuperTable/SuperTable/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'SuperTable.urls'
 
+
+TEMPLATES_PATH = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -120,3 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
