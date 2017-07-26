5d723e6f3f4f97bd6b4ecfa90caca22c1aefed56
Oliver Wei <gormory@Outlook.com>
2017-07-25 23:12:12 -0700

    Combined with timeInfo.jl

--- course.jl
+++ course.jl
@@ -1,11 +1,3 @@
-using Requests
-using JSON
-include("timeInfo.jl")
-import Requests:get,json
-
-courseAPI = "http://www.sfu.ca/bin/wcm/course-outlines?"
-
-
 function get_API(inputLink,key)
   if key == ""
     link = inputLink
@@ -14,6 +6,7 @@ function get_API(inputLink,key)
   else
     link = string(inputLink,"/",key)
   end
+  println(link)
   info = getCourseInfo(link)
   if info != -1
     println(info)
@@ -27,4 +20,64 @@ function get_API(inputLink,key)
       return ele
     end
   end
+end
+
+
+type courseSchedule
+  startTime :: String
+  endTime :: String
+  days :: String
+  sectionCode :: String
+end
+
+type examSchedule
+  startTime :: String
+  endTime :: String
+  startDate ::String
+end
+
+type course
+  courseName :: String
+  campus :: String
+  class :: Array{courseSchedule}
+  exam :: examSchedule
+end
+
+function getCourseInfo(url)
+  try
+#=   json_info = json(get(string(base_url
+                                , year ,"/"
+                                , term , "/"
+                                , department , "/"
+                                , courseNumber , "/"
+                                , courseSection)))
+=#
+    json_info = json(get(url))
+    class = Array{courseSchedule}(length(json_info["courseSchedule"]))
+    for i in 1 : length(json_info["courseSchedule"])
+      class[i] = courseSchedule(json_info["courseSchedule"][i]["startTime"],
+                              json_info["courseSchedule"][i]["endTime"],
+                              json_info["courseSchedule"][i]["days"],
+                              json_info["courseSchedule"][i]["sectionCode"])
+    end
+    try
+      exam = examSchedule(json_info["examSchedule"][1]["startTime"],
+                          json_info["examSchedule"][1]["endTime"],
+                          json_info["examSchedule"][1]["startDate"])
+      return course(json_info["info"]["name"],json_info["courseSchedule"][1]["campus"],class,exam)
+    end
+    return course(json_info["info"]["name"],json_info["courseSchedule"][1]["campus"],class,examSchedule("","",""))
+  catch
+    return -1
+  end
+end
+
+function getDepartment(year,term)
+  department_info = json(get(string(base_url,year,"/",term)))
+  department_arr = Array{String}(0)
+  #add "x" to avoid SQL keywords
+  for i in department_info
+    push!(department_arr,string(i["value"],"x"))
+  end
+  return department_arr
 end
