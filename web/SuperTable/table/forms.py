afda3c40957655ce22514b205d9e884c998e5c19
Alex Li <li1995717@gmail.com>
2017-07-26 22:21:26 -0400

    add GET method, need figure out where is the variable

--- web/SuperTable/table/forms.py
+++ web/SuperTable/table/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from table.models import SearchResult
 
 class SearchForm(forms.Form):
     year = forms.IntegerField()
     semester = forms.CharField()
     course = forms.CharField()
     courseNum = forms.IntegerField()
+    
--- web/SuperTable/table/views.py
+++ web/SuperTable/table/views.py
@@ -7,7 +7,9 @@ from django.shortcuts import render
 # import re and forms for course
 import re
 from .forms import SearchForm
+from django.views.decorators.cache import cache_page
 
+@cache_page(60*15)
 def index(request):
     yearList = ["2017","2018"]
     semseterList = ["Spring","Summer","Fall"]
@@ -16,6 +18,8 @@ def index(request):
 
 
 def get_course(request,year,semester,courseList):
-    courses = courseList.split(";")
-    rtnDict = {"year":year, "semester":semester, "course":courses}
-    return HttpResponse(rtnDict.values())    
+    if request.method == 'GET':
+        print(courseList)
+        courses = courseList.split(";")
+        rtnDict = {"year":year, "semester":semester, "course":courses}
+    return render(request, 'result.html',{"rtnDict":rtnDict})    
