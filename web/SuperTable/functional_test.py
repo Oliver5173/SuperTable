cc93ce6c390f83391bd0e825c3825b2dd5ee5d8a
Alex Li <li1995717@gmail.com>
2017-07-27 15:10:45 -0400

    add a test case for index page

--- web/SuperTable/functional_test.py
+++ web/SuperTable/functional_test.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'SuperTable' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()  
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Check out homepage
+        self.browser.get('http://localhost:8000')
+
+        # Notices the page title
+        self.assertIn('SuperTable', self.browser.title)
+        # self.fail("Finish the test!")
+
+    def tearDown(self):
+        self.browser.close()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+    
