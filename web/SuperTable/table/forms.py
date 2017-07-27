0da2976f27c58193c531032d2ecf2c286dcebe07
Alex Li <li1995717@gmail.com>
2017-07-27 15:43:37 -0400

    need to fix git ignore.

--- web/SuperTable/table/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from table.models import SearchResult
-
-class SearchForm(forms.Form):
-    year = forms.IntegerField()
-    semester = forms.CharField()
-    courseInfo = forms.CharField()
--- web/SuperTable/table/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import resolve
-from table.views import index
-# Create your tests here.
-
-class IndexPageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, index)
