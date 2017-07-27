e6e4a1cefb3deae96892bf9e6a701e97e2f2732d
Alex Li <li1995717@gmail.com>
2017-07-27 14:44:41 -0400

    add functional_test.py

--- web/SuperTable/table/views.py
+++ web/SuperTable/table/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 # import re and forms for course
 import re
 from .forms import SearchForm
+from .models import SearchRequest
 from django.views.decorators.cache import cache_page
 
 @cache_page(60*15)
