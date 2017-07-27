770e1445ac8b36a7e8f115d457422e0fde727594
Alex Li <li1995717@gmail.com>
2017-07-27 11:08:40 -0400

    finished POST method. next step is use form

--- web/SuperTable/table/forms.py
+++ web/SuperTable/table/forms.py
@@ -4,6 +4,4 @@ from table.models import SearchResult
 class SearchForm(forms.Form):
     year = forms.IntegerField()
     semester = forms.CharField()
-    course = forms.CharField()
-    courseNum = forms.IntegerField()
-    
+    courseInfo = forms.CharField()
--- web/SuperTable/table/views.py
+++ web/SuperTable/table/views.py
@@ -11,15 +11,17 @@ from django.views.decorators.cache import cache_page
 
 @cache_page(60*15)
 def index(request):
-    yearList = ["2017","2018"]
+    yearList = [2017,2018]
     semseterList = ["Spring","Summer","Fall"]
     infoDict = {"yearList":yearList,"semseterList":semseterList}
     return render(request,'home.html', {"infoDict":infoDict})
 
 
-def get_course(request,year,semester,courseList):
-    if request.method == 'GET':
-        print(courseList)
-        courses = courseList.split(";")
-        rtnDict = {"year":year, "semester":semester, "course":courses}
-    return render(request, 'result.html',{"rtnDict":rtnDict})    
+def get_course(request):
+    if request.POST:
+        year = request.POST.get('year',None)
+        semester = request.POST.get('semester',None)
+        courseList = request.POST.get('courseList',None)
+        courseList = courseList.split(";")
+        rtnDict = {"year":year, "semester":semester, "courseList":courseList}
+    return render(request, 'result.html', {"rtnDict":rtnDict})    
